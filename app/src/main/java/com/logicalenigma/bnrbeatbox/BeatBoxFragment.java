986c6c48db9e645a81db1f508b4bf0d59a620a51
David Nachman <david@nachman.org>
2015-09-07 15:57:04 -0400

    end of 18 - gridlayout hooked up with assets folder

--- app/src/main/java/com/logicalenigma/bnrbeatbox/BeatBoxFragment.java
+++ app/src/main/java/com/logicalenigma/bnrbeatbox/BeatBoxFragment.java
@@ -10,13 +10,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import java.util.List;
+
 /**
  * Copyright 2015 Logical Enigma
  * Created by dnachman on 9/7/15.
  */
 public class BeatBoxFragment extends Fragment{
+    private BeatBox mBeatBox;
+
     public static BeatBoxFragment newInstance() {
         return new BeatBoxFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        mBeatBox = new BeatBox(getActivity());
     }
 
     @Nullable
@@ -26,20 +37,31 @@ public class BeatBoxFragment extends Fragment{
 
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.fragment_beat_box_recycler_view);
         recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
-        recyclerView.setAdapter(new SoundAdapter());
+        recyclerView.setAdapter(new SoundAdapter(mBeatBox.getSounds()));
 
         return view;
     }
 
     private class SoundHolder extends RecyclerView.ViewHolder {
         private Button mButton;
+        private Sound mSound;
+
         public SoundHolder(LayoutInflater inflater, ViewGroup container) {
             super(inflater.inflate(R.layout.list_item_sound, container, false));
             mButton = (Button) itemView.findViewById(R.id.list_item_sound_button);
         }
+        public void bindSound(Sound sound) {
+            mSound = sound;
+            mButton.setText(sound.getName());
+        }
     }
 
     private class SoundAdapter extends RecyclerView.Adapter<SoundHolder> {
+        private List<Sound> mSounds;
+
+        public SoundAdapter(List<Sound> sounds) {
+            mSounds = sounds;
+        }
 
         @Override
         public SoundHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -48,13 +70,15 @@ public class BeatBoxFragment extends Fragment{
         }
 
         @Override
-        public void onBindViewHolder(SoundHolder soundHolder, int i) {
+        public void onBindViewHolder(SoundHolder soundHolder, int position) {
+            Sound sound = mSounds.get(position);
+            soundHolder.bindSound(sound);
 
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            return mSounds.size();
         }
     }
 }
---
+++ app/src/main/java/com/logicalenigma/bnrbeatbox/Sound.java
@@ -0,0 +1,25 @@
+package com.logicalenigma.bnrbeatbox;
+
+/**
+ * Copyright 2015 Logical Enigma
+ * Created by dnachman on 9/7/15.
+ */
+public class Sound {
+    private String mAssetPath;
+    private String mName;
+
+    public Sound(String assetPath) {
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length -1];
+        mName = filename.replace(".wav", "");
+    }
+
+    public String getAssetPath() {
+        return mAssetPath;
+    }
+
+    public String getName() {
+        return mName;
+    }
+}
