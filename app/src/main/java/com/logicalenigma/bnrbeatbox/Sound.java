3279be0ff19092dbf8253cd6cc30536a72c3d033
David Nachman <david@nachman.org>
2015-09-09 17:46:43 -0400

    end of 19 - sound pool

--- app/src/main/java/com/logicalenigma/bnrbeatbox/Sound.java
+++ app/src/main/java/com/logicalenigma/bnrbeatbox/Sound.java
@@ -7,6 +7,17 @@ package com.logicalenigma.bnrbeatbox;
 public class Sound {
     private String mAssetPath;
     private String mName;
+    private Integer mSoundId;
+
+    public Integer getSoundId() {
+        return mSoundId;
+    }
+
+    public void setSoundId(Integer soundId) {
+        mSoundId = soundId;
+    }
+
+
 
     public Sound(String assetPath) {
         mAssetPath = assetPath;
